a63692e90157f3d1cda2b67153a7b85adcb21775
dpradeep42 <dpradeep42@gmail.com>
2022-03-08 14:27:17 +0530

    Added Sieve of Eratosthenes

--- src/Mathematics/SieveOfEratosthenes.java
+++ src/Mathematics/SieveOfEratosthenes.java
@@ -1,5 +1,28 @@
 package Mathematics;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SieveOfEratosthenes {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        findPrimesInRange(n);
+        sc.close();
+    }
+
+    public static void findPrimesInRange(int n) {
+        boolean[] arr = new boolean[n + 1];
+        Arrays.fill(arr, true);
+        for (int i = 2; i * i <= n; i++) {
+            if (arr[i]) {
+                for (int j = 2 * i; j <= n; j += i)
+                    arr[j] = false;
+            }
+        }
+        for (int i = 2; i <= n; i++) {
+            if (arr[i])
+                System.out.println(i);
+        }
+    }
 }
