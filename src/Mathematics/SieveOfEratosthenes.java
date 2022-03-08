f55f9d4bb2e5d4e57fb15a864a5d461dc4e1ebf5
dpradeep42 <dpradeep42@gmail.com>
2022-03-08 14:39:07 +0530

    updated Sieve of Eratosthenes

--- src/Mathematics/SieveOfEratosthenes.java
+++ src/Mathematics/SieveOfEratosthenes.java
@@ -10,19 +10,32 @@ public class SieveOfEratosthenes {
         findPrimesInRange(n);
         sc.close();
     }
-
+    /***********************************************
     public static void findPrimesInRange(int n) {
         boolean[] arr = new boolean[n + 1];
         Arrays.fill(arr, true);
         for (int i = 2; i * i <= n; i++) {
             if (arr[i]) {
-                for (int j = 2 * i; j <= n; j += i)
+                for (int j = i * i; j <= n; j += i)
                     arr[j] = false;
             }
         }
         for (int i = 2; i <= n; i++) {
             if (arr[i])
                 System.out.println(i);
+        }
+    }
+    ***********************************************/
+    public static void findPrimesInRange(int n) {
+        boolean[] arr = new boolean[n + 1];
+        Arrays.fill(arr, true);
+        for(int i = 2; i <= n; i++){
+            if(arr[i]){
+                System.out.println(i);
+                for(int j = i * i; j <= n; j += i){
+                    arr[j] = false;
+                }
+            }
         }
     }
 }
